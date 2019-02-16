a1ca07bb0517ae5ae01411bdd2dacccf4b6f620a
Peowolf9 <47566985+Peowolf9@users.noreply.github.com>
2019-02-16 21:36:47 +0200

    Add files via upload

---
+++ newpraxeis.py
@@ -0,0 +1,38 @@
+# dwse dyo arithmous kai praxeis
+number1, operation, number2, junk= input("dwse praxi").split('(')
+#emfanizei tis lexeis pou edwse o xrhsths
+print (number1,operation,number2)
+#synarthsh allaghs lexewn se arithmous
+def wordstonumber(lexi):
+ if  lexi=="zero":
+    lexi=0
+ if lexi=="one":
+    lexi=1
+ if lexi=="two":
+    lexi=2
+ if lexi=="three":
+    lexi=3
+ if lexi=="four":
+    lexi=4
+ if lexi=="five":
+    lexi=5
+ if lexi=="six":
+    lexi=6
+ if lexi=="seven":
+    lexi=7
+ if lexi=="eight":
+    lexi=8
+ if lexi=="nine":
+    lexi=9
+ return lexi
+numb1=wordstonumber(number1)
+numb2=wordstonumber(number2)
+#elegxos eidos praxis kai ektypwsh apotelesmatos
+if operation == "plus":
+    print (numb1+numb2)
+if operation=="minus":
+    print (numb1-numb2)
+if operation=="times":
+    print (numb1*numb2)
+if operation=="modulo":
+     print(numb1%numb2)
